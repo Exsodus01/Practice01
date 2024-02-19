e451e4eec56ee9eecc1fece2c3e1ebfba0b3b39a
Yun Seong Ha <zkdlxx@naver.com>
2024-02-20 03:27:13 +0900

    구조 변경후 오류 발생

--- Practice00/Double.cpp
+++ Practice00/Double.cpp
@@ -8,11 +8,13 @@ using namespace std;
  int main(void) {
 	cout << "main() : " << endl;
 
-	Alpha Flist;
+	ptr listA;
+
+	listA.createList("start",3);
+	listA.insertFront(listA.getHead(), "end", 8);
+	listA.display();
+
 	
-
-
-
 
 	cout << "~main() : " << endl;
 	return 0;
--- Practice00/Doubleh.h
+++ Practice00/Doubleh.h
@@ -1,6 +1,284 @@
 #pragma once
+
 #include <string>
 using namespace std;
+
+
+class Node {
+private:
+	string Cdata;
+	int Ndata;
+
+	Node* Prev;
+	Node* Next;
+
+
+public:
+	Node()
+		:Cdata(), Ndata(0), Prev(nullptr), Next(nullptr)
+	{
+
+	}
+	~Node()
+	{
+
+	}
+
+	string getCdata(Node* head) {
+		
+		return head->Cdata;
+	}
+	int getNdata(Node* head) {
+		return head->Ndata;
+	}
+
+	Node* getPrev(Node* head) {
+		return head->Prev;
+	}
+	Node* getNext(Node* head) {
+		return head->Next;
+	}
+
+
+	Node* createNode(string Cd, int Nd) {
+		Node* Tmp = new Node;
+		Tmp->Cdata = Cd;
+		Tmp->Ndata = Nd;
+
+		return Tmp;
+	}
+
+
+	void cntSetNode(Node* head,string Cd,int Nd,int k) {
+		for (int n = 1; n < k; n++) {
+			head = head->Next;
+		}
+		head->Cdata = Cd;
+		head->Ndata = Nd;
+
+	}
+
+
+	Node* insertFront(Node* &head, string Cd,int Nd) {
+		Node* Tmp = new Node;
+		Tmp->Cdata = Cd;
+		Tmp->Ndata = Nd;
+
+
+		if (head->Prev != nullptr) {
+			head->Prev->Next = Tmp;
+			Tmp->Prev = head->Prev;
+			Tmp->Next = head;
+			head->Prev = Tmp;
+
+		}
+		else {
+			Tmp->Next = head;
+			head->Prev = Tmp;
+			head = Tmp;
+
+		}
+
+		return Tmp;
+
+	}
+
+	void insertBack(Node* &head,Node* &tail, string Cd, int Nd) {
+		Node* Tmp = new Node;
+		Tmp->Cdata = Cd;
+		Tmp->Ndata = Nd;
+
+		if (head->Next != nullptr) {
+			head->Next->Prev = Tmp;
+			Tmp->Prev = head;
+			Tmp->Next = head->Next;
+			head->Next = Tmp;
+			
+		}
+		else {
+			head->Next = Tmp;
+			Tmp->Prev = head;
+			tail = Tmp;
+		}
+
+
+	}
+
+
+
+};
+
+class ptr : public Node {
+
+private:
+	int Cnt;
+
+	Node* head;
+	Node* tail;
+
+public:
+	ptr()
+		:Cnt(0), head(nullptr), tail(nullptr)
+	{
+
+	}
+	~ptr()
+	{
+
+	}
+	int getCnt() {
+		return Cnt;
+	}
+
+	Node* getHead() {
+		return head;
+	}
+	Node* getTail() {
+		return tail;
+	}
+
+
+	void createList(string Cd,int Nd) {
+		Node* Tmp = new Node;
+		Tmp=createNode(Cd, Nd);
+		head = Tmp;
+		tail = Tmp;
+	}
+
+	void display() {
+	
+		Node* Tmp = getHead();
+
+		if (Tmp == nullptr) {
+			cout << "텅 빈 리스트입니다." << endl;
+			
+
+		}
+		else {
+			while (Tmp != nullptr) {
+				
+				cout << "Cdata : "<<getCdata(Tmp)<< endl;
+				cout << "Ndata : " << getNdata(Tmp) << endl;
+				cout << "Prev : " << getPrev(Tmp) << endl;
+				cout << "Next : " << getNext(Tmp) << endl;
+
+				Tmp = getNext(Tmp);
+			}
+
+		}
+
+
+	}
+
+
+};
+
+
+
+
+
+
+/*
+class listDouble {
+private:
+	//index 관리를 위한 변수
+	int Cnt;
+
+	class Node* Head;
+	class Node* Tail;
+
+public:
+
+	listDouble()
+		:Cnt(0), Head(nullptr), Tail(nullptr)
+	{
+		cout << "listDouble() : " << endl;
+
+	}
+	~listDouble()
+	{
+		cout << "~listDouble() : " << endl;
+	}
+
+	int getCnt() {
+		return Cnt;
+	}
+
+	Node* getHead() {
+		return Head;
+	}
+	Node* getTail() {
+		return Tail;
+	}
+
+	void setCnt(int Tmp) {
+		Cnt = Tmp;
+	}
+	void setHead(Node* head) {
+		Head = head;
+	}
+	void setTail(Node* tail) {
+		Tail = tail;
+	}
+	void display() {
+		while (!Head) {
+			cout << "Cdata : " <<  << endl;
+
+		}
+	}
+
+};
+
+class Node : public listDouble {
+
+private:
+
+	string Cdata;
+	int Ndata;
+
+	Node* Prev;
+	Node* Next;
+
+public:
+	Node()
+		:Cdata(),Ndata(0),Prev(nullptr),Next(nullptr)
+	{
+		cout << "Node() : " << endl;
+	}
+	~Node()
+	{
+		cout << "~Node() : " << endl;
+	}
+
+	void createList() {
+		Node* Tmp = new Node;
+		setHead(Tmp);
+		setTail(Tmp);
+
+	}
+
+	Node* createNode(Node* head) {
+		Node* Tmp = new Node;
+
+		return Tmp;
+	}
+
+
+	void insertFront(Node* head) {
+		Node* Tmp = new Node;
+		if (!head) {
+		
+		}
+
+	}
+
+
+
+
+};
+*/
+
+/*
 class NODE {
 public:
 	string Cdata;
@@ -10,7 +288,7 @@ public:
 	NODE* Next;
 
 	NODE()
-		:Cdata("null"),Ndata(0),Prev(nullptr),Next(nullptr)
+		:Ndata(0),Prev(nullptr),Next(nullptr)
 	{
 		cout << "NODE() : " << endl;
 	}
@@ -23,16 +301,39 @@ public:
 	
 		cout << "~NODE() : " << endl;
 	}
-	int CreateList(Alpha &list,string Cdata,int Ndata) {
+	int CreateList(Alpha &list,string Cd,int Nd) {
 
 		
 		NODE* Tmp = new NODE;
-		Tmp->Cdata = Cdata;
-		Tmp->Ndata = Ndata;
+		Tmp->Cdata = Cd;
+		Tmp->Ndata = Nd;
 		Tmp->Prev = nullptr;
 		Tmp->Next = nullptr;
 		list.SetHead(Tmp);
 		list.SetTail(Tmp);
+
+	}
+
+	int InserFront(NODE* &head, string Cd, int Nd) {
+		
+		
+		NODE* Tmp = new NODE;
+		Tmp->Cdata = Cd;
+		Tmp->Ndata = Nd;
+		if (head->Prev != nullptr) {
+			head->Prev->Next = Tmp;
+			Tmp->Prev = head->Prev;
+			
+		}
+		else {
+			Tmp->Prev = nullptr;
+			head->Prev = Tmp;
+		}
+		
+		Tmp->Next = head;
+
+
+
 
 	}
 };
@@ -63,7 +364,17 @@ public:
 	int SetTail(NODE* Tmp) {
 		Tail = Tmp;
 	}
-	int NodeSort(NODE* head) {
+	NODE* Gethead() {
+
+		return head;
+	}
+	NODE* GetTail() {
+
+		return Tail;
+	}
+
+	
+	int NodeCnt(NODE* head) {
 		while (head != nullptr) {
 			
 			head = head->Next;
@@ -72,8 +383,8 @@ public:
 
 		return Cnt;
 	}
-	NODE* Shead(NODE* head, string Cd = "null", int Nd = 0, int Cn = 0) {
-		if (Cd != "null") {
+	NODE* Shead(NODE* head,string Cd , int Nd = 0, int Cn = 0) {
+		if (Cd.empty() != true) {
 			while (head != nullptr) {
 				if (head->Cdata == Cd) {
 					cout << "Cdata를 가진 노드 탐색 성공" << endl;
@@ -118,7 +429,7 @@ public:
 		}
 		else {
 			cout << "검색 값이 없어서 노드 순서만 정렬합니다." << endl;
-			NodeSort(head);
+			NodeCnt(head);
 			return 0;
 		}
 		
@@ -136,3 +447,5 @@ public:
 
 
 };
+
+*/
